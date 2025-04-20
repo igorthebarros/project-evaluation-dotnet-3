1140a458f8afd3bf71f09e542bf1c9c15131d8bb
igorthebarros <1igorhbarros@gmail.com>
2025-04-20 12:42:23 -0300

    :sparkles: Add domain models for Consolidation and Transaction

---
+++ CashFlowControlSystem/0.Core/ConsolidationService/Domain/Entities/DailyBalance.cs
@@ -0,0 +1,13 @@
+﻿using SharedKernel.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolidationService.Domain.Entities
+{
+    public class DailyBalance : BaseEntity
+    {
+    }
+}
---
+++ CashFlowControlSystem/0.Core/TransactionService/Domain/Entities/Merchant.cs
@@ -0,0 +1,13 @@
+﻿using SharedKernel.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransactionService.Domain.Entities
+{
+    public class Merchant : BaseEntity
+    {
+    }
+}
