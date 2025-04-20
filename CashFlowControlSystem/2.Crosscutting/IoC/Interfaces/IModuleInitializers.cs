a0b596c95b66fdfe590babb2c3b8ddf9a2bea0df
igorthebarros <1igorhbarros@gmail.com>
2025-04-20 12:47:09 -0300

    :sparkles: Add IoC implementation

---
+++ CashFlowControlSystem/2.Crosscutting/IoC/Interfaces/IModuleInitializers.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace IoC.Interfaces
+{
+    public interface IModuleInitializer
+    {
+        void Initialize(WebApplicationBuilder builder);
+    }
+}
